a1483ba8e2fa7db2e3efd8dc4490b80909788df9
Barry <bsia@mocana.com>
2014-02-24 12:09:56 -0800

    Break apart lines into arrays

--- lib/linecomber/comber.rb
+++ lib/linecomber/comber.rb
@@ -2,7 +2,12 @@
 module Linecomber
   class Comber
     def self.comb(lines, filters)
-      lines
+     
+      ret = ""
+      lines.split("\n").each do |l|
+        ret = ret + l 
+      end
+      ret
     end
   end
 end
--- spec/linecomber_spec.rb
+++ spec/linecomber_spec.rb
@@ -1,17 +1,29 @@
 require 'linecomber/comber'
 
 describe Linecomber::Comber do
-  it "lines are empty" do
-    Linecomber::Comber.comb("",[]).should eql("")
+  context "when lines are empty" do
+    it "should return blank lines" do
+      Linecomber::Comber.comb("",[]).should eql("")
+    end
+    it "it should return blank when filter has one entry" do
+      Linecomber::Comber.comb("", [ /Class descriptor.*;/]).should eql("")
+    end
   end
 
-  it "lines are empty, filter has one entry" do
-    Linecomber::Comber.comb("", [ /Class descriptor.*;/]).should eql("")
-  end
 
-  it "one line, filter has no entry" do
+  it "should return original line when filter has no entry" do
     Linecomber::Comber.comb("Display this", [ ]).should eql("Display this")
   end
+
+  context "when there is a beginMark-endMark filter" do
+    filter = { :begin => /^Class.*{/, :end => /^};/ }
+    it "should return only contents within begin and end matchers" do
+      data = "Bogus\nClass something {\n\n};Bogus At End"
+      Linecomber::Comber.comb(data, [ filter ]).should eql("Class something {\n\n};")
+    end
+
+  end
+
 
 end
 
