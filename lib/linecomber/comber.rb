40af33ebc1f16dfe5cacc8b8e95b0a7c68ef7819
Barry <bsia@mocana.com>
2014-02-24 14:21:51 -0800

    Add processors

--- lib/linecomber/comber.rb
+++ lib/linecomber/comber.rb
@@ -2,15 +2,53 @@
 module Linecomber
   class Comber
     def self.comb(lines, filters)
-      ret = ""
-      capture = true if filters.length > 0 
+      collated = ""
+      capture = false
+      @@processors = [ method(:begin_processor), method(:print_processor), method(:end_processor) ]
+      init_begin_end_processors
       lines.split("\n").each do |l|
-        if capture then
-          ret = ret + l
-        end
+        ret = process_line(l, filters, @@processors)
+        collated = "#{collated}#{ret}\n" unless ret.nil?
+      end
+      collated 
+    end
+
+    def self.process_line(line, filters, processors)
+      ret = ""
+      processors.each do |p|
+        ret = p.call(line, filters)
+        return nil if ret.nil?
+        return ret unless ret.eql? line
       end
       ret
     end
+
+    def self.init_begin_end_processors
+      @@begin_printing = false
+    end
+
+    def self.begin_processor(line, filters)
+      if line =~ filters[:begin] then
+        @@begin_printing = true
+      end
+      line
+    end
+
+    def self.end_processor(line, filters)
+      if line =~ filters[:end] then
+        @@begin_printing = false
+      end
+      line
+    end
+
+    def self.print_processor(line, filters)
+      if @@begin_printing then
+        line
+      else
+        nil 
+      end
+    end
+
   end
 end
 
--- spec/linecomber_spec.rb
+++ spec/linecomber_spec.rb
@@ -3,23 +3,23 @@ require 'linecomber/comber'
 describe Linecomber::Comber do
   context "when lines are empty" do
     it "should return blank lines" do
-      Linecomber::Comber.comb("",[]).should eql("")
+      Linecomber::Comber.comb("",{}).should eql("")
     end
     it "it should return blank when filter has one entry" do
-      Linecomber::Comber.comb("", [ /Test/]).should eql("")
+      Linecomber::Comber.comb("", { :something => /Test/ }).should eql("")
     end
   end
 
 
   it "should return blank line when filter has no entry" do
-    Linecomber::Comber.comb("Display this", [ ]).should eql("")
+    Linecomber::Comber.comb("Display this", {}).should eql("")
   end
 
   context "when there is a beginMark-endMark filter" do
     filter = { :begin => /^Class.*{/, :end => /^};/ }
-    it "should return only contents within begin and end matchers" do
-      data = "Bogus\nClass something {\n\n};Bogus At End"
-      Linecomber::Comber.comb(data, [ filter ]).should eql("Class something {\n\n};")
+    it "should return only lines within begin and end matchers" do
+      data = "Bogus\nClass something {\nint x;\n};\nBogus At End\n"
+      Linecomber::Comber.comb(data, filter).should eql("Class something {\nint x;\n};\n")
     end
 
   end
